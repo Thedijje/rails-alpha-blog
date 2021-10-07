a2e3e116a5318278c4635cbfb787b4404d3d0766
Dheeraj jha <dheeraj@thedijje.com>
2021-10-07 18:22:59 +0530

    Edit and created done with update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_param)
+        @article.user = User.last
         if @article.save
             redirect_to @article
             flash[:notice] = "Article was created successfully."
