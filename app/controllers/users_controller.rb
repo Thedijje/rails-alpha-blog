779743f9beeb6a963583272f477b1901a0d03170
Dheeraj jha <dheeraj@thedijje.com>
2021-10-08 00:36:18 +0530

    user edit functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
 
     def new
-        @users  =   User.new
+        @user  =   User.new
         render 'new'
+    end
+
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice]  =   "Account info updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
     end
 
     def create
